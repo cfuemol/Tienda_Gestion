7960f1b2e6e9ac31ea3ccdc0ebc0b4a9b3a5a3f5
cfuemol <cfuemol584@g.educaand.es>
2025-05-18 14:46:01 +0200

    Implementado edición y borrado de productos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
 from datetime import date, datetime
 from models.usuario import Usuario
@@ -101,6 +101,33 @@ def detalle_producto(id_producto):
     if producto:
         return render_template('detalle_producto.html', producto=producto)
 
+# Editar productos
+@app.route('/productos/<id_producto>/editar', methods=['POST'])
+def editar_producto(id_producto):
+    nombre = request.form['nombre'].title()
+    precio = float(request.form['precio'])
+    categoria = request.form['categoria'].title()
+    stock = int(request.form['stock'])
+
+    productos_col.update_one(
+        {'_id' : ObjectId(id_producto)},
+        {'$set' :
+            {'nombre': nombre,
+             'precio': precio,
+             'categoria': categoria,
+             'stock': stock,}
+        }
+    )
+
+    return redirect(url_for('detalle_producto', id_producto=id_producto))
+
+# Borrar productos
+@app.route('/productos/<id_producto>/borrar', methods=['POST'])
+def borrar_producto(id_producto):
+    productos_col.delete_one({'_id': ObjectId(id_producto)})
+    return redirect(url_for('lista_productos', id_producto=id_producto))
+
+
 # Añadir usuarios a la BBDD
 @app.route('/registro-usuario', methods=['GET', 'POST'])
 def registro_usuario():
@@ -109,11 +136,10 @@ def registro_usuario():
     if request.method == 'POST':
         nombre_user = request.form['nombre_user'].title()
         email_user = request.form['email_user'].lower()
-        fecha = datetime.today()
+        fecha_user = datetime.today().strftime('%d/%m/%Y')
         activo_user = request.form['activo_user']
         passw_user = request.form['passw_user']
 
-        fecha_user = fecha.strftime('%d/%m/%Y')
 
         usuario = Usuario(nombre_user, email_user, passw_user, activo_user,fecha_user)
 
@@ -129,6 +155,50 @@ def lista_usuario():
     clientes = list(usuarios_col.find())
     return render_template('lista_usuarios.html', lista_clientes=clientes)
 
+# Añadir pedidos
+@app.route('/add_pedido', methods=['GET', 'POST'])
+def add_pedido():
+    mensaje = ''
+    clientes = list(usuarios_col.find())
+
+    if request.method == 'POST':
+
+        cliente_id = request.form.get('cliente', '').strip()
+        total = float(request.form.get('total', 0))
+
+        if not cliente_id:
+            mensaje = 'Selecciona un cliente'
+        else:
+            try:
+                cliente = usuarios_col.find_one({'_id': ObjectId(cliente_id)})
+
+                if not cliente:
+                    mensaje = 'El cliente no existe'
+                else:
+                    pedido= {
+                        'cliente' : cliente['nombre'],
+                        'total' : total,
+                        'fecha' : datetime.today().strftime('%d/%m/%Y'),
+                    }
+
+                    print(f'Inserto pedido: {pedido}')
+
+                    pedidos_col.insert_one(pedido)
+
+                    # Le sumamos 1 al campo num_pedidos del cliente
+                    usuarios_col.update_one(
+                        {'_id': cliente['_id']},
+                        {'$inc': {'pedidos': 1}}
+                    )
+
+                    mensaje = 'Pedido añadido correctamente'
+
+            except Exception as e:
+                mensaje = f'Error no determinado: {e}'
+
+    return render_template('add_pedido.html', mensaje=mensaje, clientes=clientes)
+
+
 # Manejador de error 404 - Page not found
 @app.errorhandler(404)
 def not_found(e):
@@ -138,8 +208,6 @@ def not_found(e):
 @app.errorhandler(500)
 def not_found(e):
     return render_template('404.html', mensaje='Error en el servidor')
-## Crear los endpoints a la misma vez que las webs
-# correspondientes y en el nav##
 
 
 if __name__ == '__main__':
